20353efd99e1ee58394334abf411d2243ae73bfa
yoshidanoryota <yoshidanoryota777x@gmail.com>
2021-07-30 19:47:08 +0900

    新規登録・ログイン機能の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
-
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+ 
+  
   private
 
   def basic_auth
@@ -9,6 +12,11 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name ,:first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
+  end
+  
 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
 
   validates :user_name, :first_name, :last_name, :first_name_kana,
             :last_name_kana, :birthday,  presence: true
+
+  validates :encrypted_password,:password,length:
+            {minimum:6},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
 end
