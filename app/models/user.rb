d4fb20e82209732f7dbe83c80627f13bc9b48440
yoshidanoryota <yoshidanoryota777x@gmail.com>
2021-08-02 12:53:21 +0900

    ユーザー管理機能　修正③

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :user_name,:birthday
       with_options format: {with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/} do
-        validates :encrypted_password,:password
+        validates :password
       end
       with_options format: {with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/,} do
         validates :first_name_kana, :last_name_kana
