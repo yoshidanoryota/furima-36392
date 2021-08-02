20a9d9492ad6b796c60b432aafe9f21ac3bff950
yoshidanoryota <yoshidanoryota777x@gmail.com>
2021-08-02 17:59:13 +0900

    createアクション調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
 
   def index
   end
@@ -9,15 +10,16 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.save
-    redirect_to root_path
-
+    if @item.save
+       redirect_to root_path
+    else
+       render :new
+    end
   end
 
   private
   def item_params
     params.require(:item).permit(:name, :price, :introduction, :category_id, :status_id, :postage_id, :area_id, :shipping_date_id ,:image).merge(user_id: current_user.id)
   end
-
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,10 +10,11 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name, :price, :introduction, :user, :category_id,
               :status_id, :postage_id, :area_id, :shipping_date_id, :image
+    validates :category_id, :status_id, :postage_id, :area_id, :shipping_date_id,
+              numericality: { other_than: 1 , message: "can't be blank"}
   end
 
-  validates :category_id, :status_id, :postage_id, :area_id, :shipping_date_id,
-             numericality: { other_than: 1 , message: "can't be blank"}
+  validates_inclusion_of :price, in:300..9999999 
 
 end
 
