6c9fb8ae5e02761b7a8ba98ba961b884bce06a0c
yoshidanoryota <yoshidanoryota777x@gmail.com>
2021-08-02 17:06:22 +0900

    ActiveStorageの導入及び設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,7 +16,7 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :price, :introduction, :category_id, :status_id, :postage_id, :area_id, :shipping_date_id ).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :price, :introduction, :category_id, :status_id, :postage_id, :area_id, :shipping_date_id ,:image).merge(user_id: current_user.id)
   end
 
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,13 +2,14 @@ class Item < ApplicationRecord
 
   has_one :purchase
   belongs_to :user
+  has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :status, :postage, :area, :shipping
 
   with_options presence: true do
     validates :name, :price, :introduction, :user, :category_id,
-              :status_id, :postage_id, :area_id, :shipping_date_id
+              :status_id, :postage_id, :area_id, :shipping_date_id, :image
   end
 
   validates :category_id, :status_id, :postage_id, :area_id, :shipping_date_id,
