677ad8178ee30d54b73604d701260cc13f93c420
yoshidanoryota <yoshidanoryota777x@gmail.com>
2021-08-03 17:11:17 +0900

    rubocopの実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,7 +16,7 @@ class Item < ApplicationRecord
     validates :category_id, :status_id, :postage_id, :area_id, :shipping_date_id,
               numericality: { other_than: 1, message: "can't be blank" }
     validates_inclusion_of :price, in: 300..9_999_999
-    with_options format: { with: /\A[0-9]+\z/} do
+    with_options format: { with: /\A[0-9]+\z/ } do
       validates :price
     end
   end
