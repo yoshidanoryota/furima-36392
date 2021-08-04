f1db71c2b5efb037fb76a93bb0cd6985ce10679b
yoshidanoryota <yoshidanoryota777x@gmail.com>
2021-08-04 15:14:11 +0900

    Rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,7 +34,6 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
-
   end
 
   private
@@ -46,10 +45,6 @@ class ItemsController < ApplicationController
 
   def move_to_index
     item = Item.find(params[:id])
-    unless user_signed_in? && current_user.id == item.user_id
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless user_signed_in? && current_user.id == item.user_id
   end
-
-
 end
